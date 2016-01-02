109868cf2e934f3aa0c7a35d6d476d7b3f5b4d50
Francisco Miranda <francisco@tutum.co>
2016-01-02 18:03:34 +0100

    Add all HTTP methods to factory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import { get, post, patch, del } from 'superagent';
-import each from 'lodash/collection/each';
+import { get, post, put, del, patch } from 'superagent';
+import partial from 'lodash/function/partial';
 
-export default function factory(baseUri, headers){
-  return (subpath) => {
-    const getter = get(`${baseUri}${subpath}`);
-    each(headers, (val, key) => getter.set(key, val));
-    return getter.end.bind(getter);
+function requestFactory (baseUri, headers, fn) {
+  return path => {
+    const req = fn(`${baseUri}${path}`);
+    Object.keys(headers).forEach(key => req.set(key, headers[key]));
+    return req.end.bind(req);  
   }
 }
 
+function factory(baseUri, headers){
+  const requestor = partial(requestFactory,baseUri,headers);
+
+  // Not doing api = getFn
+  // to avoid circular references
+  const api = (...args) => requestor(get)(...args);
+
+  api.get = requestor(get);
+  api.post = requestor(post);
+  api.put = requestor(put);
+  api.del = requestor(del);
+  api.patch = requestor(patch);
+
+  return api;
+}
+
+export default factory;
--- test/base.js
+++ test/base.js
@@ -2,24 +2,73 @@
 /* eslint-disable no-unused-expressions */
 
 import { expect } from 'chai';
-import sdker from '../src';
+import sdktor from '../src';
 
 const AUTH = `Basic ${new Buffer('maroshii:2e024c866f8e9bbbee78cb6957cbf85c752d64c7639ea51b').toString('base64')}`;
-const sdk = sdker('https://dashboard.tutum.co/api/v1', {
+const sdk = sdktor('https://dashboard.tutum.co/api/v1', {
   Accept: 'application/json',
   Authorization: AUTH,
   Host: 'dashboard.tutum.co',
 });
 
+const assertCollection = ({ body }) => {
+  expect(body).to.be.an('object');
+  expect(body.meta).to.be.an('object');
+  expect(body.objects).to.be.an('array');
+}
+
+
 describe('Requests', () => {
-  describe('default', () => {
+  describe('Basic HTTP Methods', () => {
     it('defaults to GET', (done) => {
-      const getter = sdk('/service');
-      getter(function(err, data) {
-        console.log(data.body.meta);
+      const get = sdk('/service/');
+      get(function(err, data) {
+        if(err) return done(err);
+        assertCollection(data);
         done();
       });
     });
+    it('get()', (done) => {
+      const get = sdk.get('/service/');
+      get(function(err, data) {
+        if(err) return done(err);
+        assertCollection(data);
+        done();
+      });
+    });
+    it('post()', (done) => {
+      const post = sdk.post('/service/');
+      post(function(err, data) {
+        expect(err.message).to.equal('Bad Request');
+        expect(err.status).to.equal(400);
+        done();
+      });
+    });
+    it('patch()', (done) => {
+      const post = sdk.patch('/service/');
+      post(function(err, data) {
+        expect(err.message).to.equal('Method Not Allowed');
+        expect(err.status).to.equal(405);
+        done();
+      });
+    });
+    it('put()', (done) => {
+      const post = sdk.put('/service/');
+      post(function(err, data) {
+        expect(err.message).to.equal('Method Not Allowed');
+        expect(err.status).to.equal(405);
+        done();
+      });
+    });
+    it('del()', (done) => {
+      const post = sdk.del('/service/');
+      post(function(err, data) {
+        expect(err.message).to.equal('Method Not Allowed');
+        expect(err.status).to.equal(405);
+        done();
+      });
+    });
+
   });
 });
 /* eslint-enable no-unused-expressions */
